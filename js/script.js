3d25a69086828289ae0f60b3930d4ab1316295be
laulaunidas <laulaunidas@gmail.com>
2018-07-10 13:08:03 +0200

    optimisation js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 // navigation
-const links = document.getElementsByClassName("navigation__link");
+const links = document.querySelectorAll(".navigation__link");
 const naviToggle = document.getElementById("navi-toggle");
-const linksTable = [...links];
 
 // pour lien de la nav, on ajoute un listener de click
 // une fois cliqué on met l'état de la checkbox à false pour fermer la feneter de navigation
-linksTable.forEach(link => {
+links.forEach(link => {
   link.addEventListener("click", () => {
     naviToggle.checked = false;
   });
@@ -13,10 +12,5 @@ linksTable.forEach(link => {
 
 //fermer la modal du booking
 const modalBooking = document.getElementById("popup");
-window.onclick = event => {
-  if (event.target == modalBooking) {
-    modalBooking.style.display = "none";
-  } else {
-    modalBooking.style.display = "block";
-  }
-};
+window.onclick = event => event.target === modalBooking ? modalBooking.style.display = "none" : modalBooking.style.display = "block" ;
+
